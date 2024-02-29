5f0626d2825019dcc77420adfa1c3f825d1d7a81
aliramzan786 <arbuiltinsoft@gmail.com>
2024-02-29 19:02:54 +0500

    objects done

--- Javascript/events/app.js
+++ Javascript/events/app.js
@@ -2,5 +2,5 @@ const handleClick = (event) => {
     console.log('button click event is triggered')
 }
 const button = document.querySelector('button');
-if(button) button.addEventListener('click', handleClick)
+if(button) button.addEventListener('mouseenter', handleClick)
 // button.addEventListener('click', handleClick)
