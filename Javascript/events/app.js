0a7d7017069db1c374fc1e202d91acde26d2e27d
aliramzan786 <arbuiltinsoft@gmail.com>
2024-02-29 19:42:11 +0500

    basic events done

--- Javascript/events/app.js
+++
@@ -1,6 +0,0 @@
-const handleClick = (event) => {
-    console.log('button click event is triggered')
-}
-const button = document.querySelector('button');
-if(button) button.addEventListener('mouseenter', handleClick)
-// button.addEventListener('click', handleClick)
